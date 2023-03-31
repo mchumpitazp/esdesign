0b7d2ce82793cef70f72d7379a420ba93255b718
Mauro Chumpitaz Polino <mchumpitazp@gmail.com>
2023-03-31 20:26:46 +0300

    init client

---
+++ client/src/components/MainComponent.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+// Components
+import Header from "./HeaderComponent";
+
+// Redux
+
+function Main () {
+
+    return (
+        <React.Fragment>
+            <Header />
+        </React.Fragment>
+    );
+}
+
+export default Main;
