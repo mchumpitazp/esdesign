35472a6e55c1b021644d49c1b561d02676974351
Mauro Chumpitaz Polino <mchumpitazp@gmail.com>
2023-04-07 22:45:30 +0300

    Set Colors

--- client/src/components/TestimonalsComponent.js
+++ client/src/components/TestimonalsComponent.js
@@ -8,7 +8,7 @@ function Testimonials () {
                 <h2 className="section-header">Testimonios</h2>
 
                 <Row xs="1" md="2">
-                    <Col data-aos="fade-up" data-aos-delay="0">
+                    <Col data-aos="fade-up" data-aos-delay="200">
                         <div className="testimonial">
                             <p>"Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean."</p>
                             <div>
@@ -17,7 +17,7 @@ function Testimonials () {
                             </div>
                         </div>
                     </Col>
-                    <Col data-aos="fade-up" data-aos-delay="100">
+                    <Col data-aos="fade-up" data-aos-delay="300">
                         <div className="testimonial">
                             <p>"Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean."</p>
                             <div>
